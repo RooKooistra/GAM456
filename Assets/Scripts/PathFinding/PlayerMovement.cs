d95e5386de53280dfa7e1b6e72bf550aa24e22fd
roo.kooistra@gmail.com <K1ttyk4t!>
2021-07-02 23:06:50 +1000

    Steering behavior working good enough

--- Assets/Scripts/PathFinding/PlayerMovement.cs
+++ Assets/Scripts/PathFinding/PlayerMovement.cs
@@ -5,14 +5,20 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float playerRotationSpeed = 20;
-	public float playerMaxForce = 1;
-	public float playerMaxSpeed = 1;
-	public float distanceToNodeTolerance = 0.5f;
+	public float playerRotationSpeed = 10;
+	public float avoidRotationSpeed = 2;
+	public float playerMaxForce = 3;
+	public float playerMaxSpeed = 2;
+	public float distanceToNodeTolerance = 1f;
+
+	public float baseFeelerDistance = 2f;
+	public float feelerMaxDistanceForward = 5;
+	public float numberOfFeelersPerSide = 10;
+
+	public LayerMask layerMask;
 
 	public Text debugData;
 
-	//List<Node> pathToTravel = new List<Node>();
 	Rigidbody rigidBody;
 
 	private void Start()
@@ -27,54 +33,81 @@ public class PlayerMovement : MonoBehaviour
 
 	IEnumerator TravelPathGo(List<Node> path)
 	{
-		//pathToTravel = path;
-		
-		int index = 0;
+
+		int activeIndex = 0;
 		while (true)
 		{
 			
-
-			if (Vector3.Distance(transform.position, path[index].worldPosition) < distanceToNodeTolerance)
+			if (Vector3.Distance(transform.position, path[activeIndex].worldPosition) < distanceToNodeTolerance)
 			{
-				index++;
-
-				if (index >= path.Count) yield break;
+				activeIndex++;
+				if (activeIndex >= path.Count) yield break;
 			}
 
-			Vector3 targetDirection = (path[index].worldPosition - transform.position).normalized;
-
-			Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, playerRotationSpeed * Time.deltaTime, 0.0f);
-
-			Debug.DrawRay(transform.position, newDirection, Color.red);
-
-			transform.rotation = Quaternion.LookRotation(newDirection);
+			LookAtTarget(path[activeIndex].worldPosition);
+			WallCheck();
 
 			rigidBody.AddForce(transform.forward * playerMaxForce);
 			rigidBody.velocity = Vector3.ClampMagnitude(rigidBody.velocity, playerMaxSpeed);
 
-			debugData.text = path.Count.ToString();
-			index = CheckAhead(path, index); // run check head to see if any further nodes are visible
+			activeIndex = CheckAhead(path, activeIndex); // run check head to see if any further nodes are visible
 
 			yield return null;
 		}
 
 	}
 
-	int CheckAhead(List<Node> path, int currentIndex)
+	void LookAtTarget(Vector3 lookAtVector)
 	{
-		int intToReturn = currentIndex;
-		for(int i = currentIndex; i <= path.Count; i++)
-		{
-			var direction = (path[i].worldPosition - transform.position).normalized;
+		Vector3 transDirection = transform.InverseTransformPoint(lookAtVector).normalized;
+		float torqueAmount = (transDirection.z >= 0) ? transDirection.x : (transDirection.x > 0) ? 1 : -1;
+		rigidBody.AddTorque(new Vector3(0, torqueAmount * playerRotationSpeed), ForceMode.Force);
+	}
 
-			RaycastHit hit;
-			if (Physics.Raycast(transform.position, direction, out hit, float.MaxValue))
+	// make an array of raycasts small on the sides, longer forwards and push in the oposite x direction
+	void WallCheck()
+	{
+		for(int x = -1; x <= 1; x++)
+		{
+			for (float z = 0; z <= feelerMaxDistanceForward; z = z + feelerMaxDistanceForward / numberOfFeelersPerSide)
 			{
-				if (hit.transform.position == path[i].worldPosition) intToReturn = i;
+				if (x == 0) continue; // eliminate raycast straight forward
+				var rayDirection = transform.TransformDirection(new Vector3(x, 0, z)).normalized;
+				Debug.DrawRay(transform.position, rayDirection * (z + baseFeelerDistance), Color.magenta);
+				RaycastHit hit;
+				if (Physics.Raycast(transform.position, rayDirection, out hit, z + baseFeelerDistance, layerMask))
+				{
+					// rotational force divided by hit length (more force when closer) ... Longer feelers have weaker strength
+					rigidBody.AddTorque(new Vector3(0, -x / hit.distance * avoidRotationSpeed / (z+1f)), ForceMode.Force); 
+					Debug.Log("Hit");
+				}
 			}
 		}
 
-		//debugData.text = ("Index to return " + intToReturn);
+	}
+
+	int CheckAhead(List<Node> path, int currentIndex)
+	{
+		
+		int intToReturn = currentIndex;
+
+		for (int i = currentIndex; i < path.Count; i++)
+		{
+			
+
+			var direction = (path[i].worldPosition - transform.position).normalized;
+			float distance = Vector3.Distance(path[i].worldPosition, transform.position);
+
+			RaycastHit hit;
+			if (!Physics.Raycast(transform.position, direction, out hit, distance, layerMask))
+			{
+				intToReturn = i;
+			}
+			else
+			{
+				return intToReturn;
+			}
+		}
 		return intToReturn;
 	}
 
